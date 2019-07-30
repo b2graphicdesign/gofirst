9f5cdc1ac196823c52f76e0cc707d1decf18f3ef
Brian Carstensen <brian.carstensen@gmail.com>
2019-07-29 21:58:05 -0500

    Start messing with animation

--- src/components/finger.js
+++ src/components/finger.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import posed, { PoseGroup } from 'react-pose';
+
+const FingerGroup = posed.g({});
+
+const FingerCircle = posed.circle({
+  enter: { r: ({ winner, loser }) => winner ? 90 : loser ? 50 : 70 },
+  exit: { r: 0 },
+});
+
+const GROUP_STYLE = {
+  transition: 'transform 250ms cubic-bezier(0, 0, 0.5, 2)',
+};
 
 export default function Finger({
   x,
@@ -8,12 +20,14 @@ export default function Finger({
   loser
 }) {
   return (
-    <g transform={`translate(${x}, ${y})`}>
-      <circle
-        r={winner ? 90 : loser ? 50 : 70}
-        fill={`hsla(${hue}, 50%, 50%, ${winner ? 1 : 0.8})`}
-        stroke="none"
-      />
-    </g>
+    <PoseGroup animateOnMount={true}>
+      <FingerGroup key={0} transform={`translate(${x}, ${y})`} xstyle={GROUP_STYLE}>
+        <FingerCircle
+          {...{winner, loser}}
+          fill={`hsla(${hue}, 50%, 50%, ${winner ? 1 : 0.8})`}
+          stroke="none"
+        />
+      </FingerGroup>
+    </PoseGroup>
   );
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+import 'babel-polyfill';
 import 'pepjs';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
